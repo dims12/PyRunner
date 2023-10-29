d4b6510abac735de646ed095420e093192ca3b95
dims@calculon <dims12@mail.ru>
2023-10-29 20:14:18 +0300

    bump versions

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
     name='NormalPyRunner',
-    version='0.1',
+    version='0.1.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         # List your project dependencies here
